35204a682c0f69231223d7c922ce6e45b35ac546
salmontasama <salmontasama1@gmail.com>
2022-03-17 09:35:19 +0200

    Answers List

---
+++ list.1.py
@@ -0,0 +1,102 @@
+# ls = []
+# for i in range(100):
+#     ls.append(i)
+# print(ls)
+
+# ls = []
+# for i in range(10):
+#     name = input()
+#     ls.append(name)
+# for i in ls:
+#     print(i)
+
+# ls = ['salmon','dani','aviva','mordachi']
+# newls = []
+# for i in ls:
+#     if 'a'in i:
+#         newls.append(i)
+# print(newls)
+
+# ls = [1,2,3,4,5,6,7,8,9,10]
+#print(ls)
+
+# ls = []
+# for i in range(10):
+#     a = int(input('enter number'))
+#     ls.insert(i,a)
+# print(ls)
+# print(sorted(ls))
+
+# ls = []
+# even_11 = []
+# big10 = []
+# last10 = []
+# maxx = []
+# oddNumber = []
+# Couple = []
+# even3 = []
+# for i in range (3):
+#     ls.append( int(input('enter number')))
+#     if i >10:
+#         big10.append(i)
+#     else:last10.append(i)
+# for i in ls:
+#     y = max(ls)
+#     maxx.append(y)
+#     if i %2==0:
+#         oddNumber.append(i)
+#     else:Couple.append(i)
+#     if i %3==0:
+#         even3.append(i)
+# print('sum:',sum(ls))
+# print('even11:',even_11)
+# print('big10:',big10)
+# print('last10:',last10)
+# print('max:',max(ls))
+# print(oddNumber)
+# print(Couple)
+# print('even3',even3)
+
+# ls = [1,2,3,9,8,6,6,7,5,4,4]
+# for i in range(0,len(ls)-1):
+#     if ls[i] == ls[i+1]:
+#         print(("yes"))
+#     else:
+#         print("no")
+
+# ls = [3,int(input('enter number'))]
+# ls2 = [ls[0]+ls[1]]
+# ls3 = [ls[0]*ls[1]*ls2[0]]
+# ls4 = [ls,ls2,ls3]
+# for i in range(1):
+#     # ls4.append(ls,ls2,ls3)
+#
+#     print(ls)
+#     print(ls2)
+#     print(ls3)
+# for i in range(len(ls4)):
+#     print(ls4[i],end=",")
+
+
+# l1 = []
+# l2 = []
+# for i in range(5):
+#     l1.append(int(input("enter number:")))
+#     l2.append((int(input("enter num:"))))
+# for i in l1:
+#     for j in l2:
+#         if i ==j:
+#             print(i,"same scorn")
+
+# shvil = []
+# sum = 0
+# for i in range(3):
+#     name = input('name of rode:')
+#     dist = int(input("dist of rode:"))
+#     shvil.append([name,dist])
+#     sum +=dist
+#     if dist>10:
+#         print(dist)
+# for i in ls:
+#     x = i // 11
+#     even_11.append(x)
